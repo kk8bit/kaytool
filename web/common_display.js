0494f42e3453446164c0d3eec6300dc0d23e810a
kk8bit <guemu@qq.com>
2025-03-02 06:21:07 +0800

    v0.21.2

--- web/common_display.js
+++ web/common_display.js
@@ -1,14 +1,14 @@
-
 import { app } from "../../scripts/app.js";
 import { ComfyWidgets } from "../../scripts/widgets.js";
 
-export function setupGenericDisplay(nodeType, nodeName) {
+// 定义通用显示逻辑
+function setupGenericDisplay(nodeType, nodeName) {
     const onExecuted = nodeType.prototype.onExecuted;
     nodeType.prototype.onExecuted = function (message) {
-
+        // 调用原始的 onExecuted 方法（如果存在）
         onExecuted?.apply(this, arguments);
 
-
+        // 清理多余的 widgets
         if (this.widgets && this.widgets.length > 0) {
             for (let i = 1; i < this.widgets.length; i++) {
                 this.widgets[i]?.onRemove?.();
@@ -16,13 +16,13 @@ export function setupGenericDisplay(nodeType, nodeName) {
             this.widgets.length = 1;
         }
 
+        // 查找或创建 "displaytext" widget
         let textWidget = Array.isArray(this.widgets)
             ? this.widgets.find(w => w && w.name === "displaytext")
             : null;
 
         if (!textWidget) {
             try {
-        
                 textWidget = ComfyWidgets["STRING"](this, "displaytext", ["STRING", { multiline: true }], app).widget;
                 if (textWidget && textWidget.inputEl) {
                     textWidget.inputEl.readOnly = true;
@@ -34,7 +34,7 @@ export function setupGenericDisplay(nodeType, nodeName) {
             }
         }
 
-
+        // 更新 "displaytext" widget 的值
         if (message && message.hasOwnProperty("text")) {
             const text = message["text"];
             if (textWidget) {
@@ -47,3 +47,14 @@ export function setupGenericDisplay(nodeType, nodeName) {
         }
     };
 }
+
+// 注册扩展
+app.registerExtension({
+    name: "KayTool.GenericDisplay",
+    async beforeRegisterNodeDef(nodeType, nodeData, app) {
+        const nodesWithDisplay = ["Display_Any", "To_Int", "Abc_Math"];
+        if (nodesWithDisplay.includes(nodeData.name)) {
+            setupGenericDisplay(nodeType, nodeData.name);
+        }
+    },
+});
